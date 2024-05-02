d704e97315cac010eb870d485da26e575645a3c5
HazaelP <73922881+HazaelP@users.noreply.github.com>
2024-05-02 14:12:34 +0200

    folder for exercises

---
+++ statefile/outputs.tf
@@ -0,0 +1,8 @@
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage_account.name
+}
+
+output "primary_web_host" {
+  value = azurerm_storage_account.storage_account.primary_web_host
+}
