9dd409b8c903d827d54727e3db9277cc66ba6001
Emanuele Verga <emanuele.verga@eficode.com>
2024-05-10 14:05:07 +0200

    Changed resource group creation to match workstation requirements

--- trainer/create-environment/02_creatererg.tf
+++ trainer/create-environment/02_creatererg.tf
@@ -1,19 +1,6 @@
 resource "azurerm_resource_group" "example" {
   count    = var.number_of_students
-  name     = "rg-workstation${count.index + 1}"
+  # This needs to match the output of modules_intenals\configuration for the exercises to run
+  name     = "rg-workstation-${count.index + 1}"
   location = "West Europe"
-}
-
-resource "azurerm_role_assignment" "example" {
-  count                = var.number_of_students
-  scope                = azurerm_resource_group.example[count.index].id
-  role_definition_name = "Contributor"
-  principal_id         = azuread_user.example[count.index].id
-}
-
-resource "azurerm_role_assignment" "keyvault" {
-  count                = var.number_of_students
-  scope                = azurerm_resource_group.example[count.index].id
-  role_definition_name = "Key Vault Data Access Administrator"
-  principal_id         = azuread_user.example[count.index].id
 }
