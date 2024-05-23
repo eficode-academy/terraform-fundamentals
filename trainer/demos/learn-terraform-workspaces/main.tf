ac9573a68c59dedc2795fde87410676adf9e4af5
noemi <noemi.csokas@eficode.com>
2024-05-23 09:21:58 +0200

    feat: Add Terraform workspace instructions and main.tf file

---
+++ trainer/demos/learn-terraform-workspaces/main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  resource_group_name = "${terraform.workspace}-rg"
+  location            = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = local.resource_group_name
+  location = local.location
+}
